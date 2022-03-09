30491e546d5f8fb56a65b8a689b12534c6afb60b
shafucheng <flynn.sha@outlook.com>
2022-03-10 03:10:28 +0800

    feat: 设置 antd, dayjs 中文

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom'
 import Router from './router'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/es/locale/zh_CN'
 
 function App() {
   return (
-    <>
+    <ConfigProvider locale={zhCN}>
       <nav>
         <Link to="/">首页</Link> | <Link to="/about">关于</Link>
       </nav>
       <Router />
-    </>
+    </ConfigProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+dayjs.locale('zh-cn')
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
