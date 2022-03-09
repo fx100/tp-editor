0c81f5e6217b9aff97c5ea7608cc3faee60256a5
shafucheng <flynn.sha@outlook.com>
2022-03-10 02:52:55 +0800

    chore: 使用 unplugin-auto-import 自动导入 react

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
+import autoImport from 'unplugin-auto-import/vite'
 import imp from 'vite-plugin-imp'
 
 export default defineConfig({
   plugins: [
     react(),
     eslint(),
+    autoImport({
+      imports: ['react'],
+      dts: './src/auto-imports.d.ts',
+    }),
     imp({
       libList: [
         {
