0831b31b4a3d511397ebb791061345f0d5c84fd8
shafucheng <flynn.sha@outlook.com>
2022-03-10 00:48:25 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = React.useState(0)
+
+  return (
+    <button type="button" onClick={() => setCount((count) => count + 1)}>
+      count is: {count}
+    </button>
+  )
+}
+
+export default App
