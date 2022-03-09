814ed65edabecb617ebbe49427f56296571f10f2
shafucheng <flynn.sha@outlook.com>
2022-03-10 02:28:51 +0800

    chore: antd 自动导入 css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
+import imp from 'vite-plugin-imp'
 
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslint(),
+    imp({
+      libList: [
+        {
+          libName: 'antd',
+          style: (name) => `antd/es/${name}/style/css`,
+        },
+      ],
+    }),
+  ],
 })
