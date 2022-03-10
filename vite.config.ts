a2ec01c0935c560ec4b8d11812a9c9a480e55d5b
shafucheng <flynn.sha@outlook.com>
2022-03-10 15:42:32 +0800

    chore(vite): serve 和 build 分开配置， serve 全量导入 build 按需导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,76 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 import autoImport from 'unplugin-auto-import/vite'
-import imp from 'vite-plugin-imp'
 import antdDayjs from 'antd-dayjs-vite-plugin'
+import imp from 'vite-plugin-imp'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    eslint(),
-    autoImport({
-      imports: ['react'],
-      dts: './src/auto-imports.d.ts',
-    }),
-    imp({
-      libList: [
-        {
-          libName: 'antd',
-          libDirectory: 'es',
-          style: (name) => `antd/es/${name}/style`,
-        },
-        {
-          libName: '@formily/antd',
-          libDirectory: 'esm',
-          style: (name) => `@formily/antd/esm/${name}/style`,
-        },
-      ],
-    }),
-    antdDayjs(),
-  ],
-  resolve: {
-    alias: {
-      '~antd': 'antd',
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
+export default defineConfig(({ command }) => {
+  const common: UserConfig = {
+    plugins: [
+      react(),
+      eslint(),
+      autoImport({
+        imports: ['react'],
+        dts: './src/auto-imports.d.ts',
+      }),
+      antdDayjs(),
+    ],
+    resolve: {
+      alias: {
+        '~antd': 'antd',
       },
     },
-  },
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        },
+      },
+    },
+  }
+
+  if (command === 'serve') {
+    return {
+      ...common,
+      plugins: [
+        ...common.plugins,
+        imp({
+          libList: [
+            {
+              libName: 'antd',
+              libDirectory: 'dist',
+              style: () => 'antd/dist/antd.css',
+            },
+            {
+              libName: '@formily/antd',
+              libDirectory: 'dist',
+              style: () => `@formily/antd/dist/antd.css`,
+            },
+          ],
+        }),
+      ],
+    }
+  } else {
+    return {
+      ...common,
+      plugins: [
+        ...common.plugins,
+        imp({
+          libList: [
+            {
+              libName: 'antd',
+              libDirectory: 'es',
+              style: (name) => `antd/es/${name}/style`,
+            },
+            {
+              libName: '@formily/antd',
+              libDirectory: 'esm',
+              style: (name) => `@formily/antd/esm/${name}/style`,
+            },
+          ],
+        }),
+      ],
+    }
+  }
 })
